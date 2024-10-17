27271dc2af7d96081a05492aad37dd672241a97c
danielol264 <dalopezmacias@gmail.com>
2024-10-17 10:07:44 -0600

    estructura de la aplicacion

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-user ="axel"
-print(user)
+from flask import Flask, request, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('login.html')
+
+
+if __name__=='__main__':
+
+    app.run(port=5000,debug=True)
